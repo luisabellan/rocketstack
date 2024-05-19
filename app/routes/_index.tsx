9bbd6c6d1672dd4a0e46215d313d798bd1f0d67b
Luis Abellan <luis.abellan.dev@gmail.com>
2024-05-19 16:34:15 +0200

    Initial commit - Added docker, supabase and hasura

---
+++ app/routes/_index.tsx
@@ -0,0 +1,22 @@
+
+import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { createClient } from "~/utils/supabase.server";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const supabase = createClient(request);
+  const { data: todos } = await supabase.from('todos').select()
+
+  return { todos }
+}
+
+export default function Index() {
+  const { todos } = useLoaderData<typeof loader>();
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>{todo.name}</li>
+      ))}
+    </ul>
+  );
+}
